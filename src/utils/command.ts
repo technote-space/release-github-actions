d5b0fc32350186400a501507d61ab2290b900432
Technote <technote.space@gmail.com>
2019-08-25 21:46:34 +0900

    test

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -17,21 +17,15 @@ export const runBuild = async () => {
     const commands = getBuildCommands();
     if (!commands.length) return;
 
-    const current = process.cwd();
-    const workspace = getWorkspace();
-    signale.info('workspace=%s', workspace);
-    signale.info('current=%s', current);
-    await execAsync(`cd ${workspace}`);
     for (const command of commands) {
         await execAsync(command);
     }
-    await execAsync(`cd ${current}`);
 };
 
 export const getDiffFiles = async () => {
     const workspace = getWorkspace();
     await execAsync(`git -C ${workspace} add --all --force`);
-    await execAsync(`git -C ${workspace} status --short -uno`);
+    signale.info(await execAsync(`git -C ${workspace} status --short -uno`));
     return (await execAsync(`git -C ${workspace} status --short -uno`)).split(/\r\n|\n/).filter(line => line.match(/^[MDA]\s+/)).map(line => line.replace(/^[MDA]\s+/, ''));
 };
 
