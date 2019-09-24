f31f7340ed28ef9578a3068a001c98dc0e213e46
Technote <technote.space@gmail.com>
2019-09-24 22:17:55 +0900

    feat: update tests

--- __tests__/utils/command1.test.ts
+++ __tests__/utils/command1.test.ts
@@ -1,63 +1,52 @@
 /* eslint-disable no-magic-numbers */
 import path from 'path';
-import { EOL } from 'os';
-import { getContext, testEnv } from '@technote-space/github-action-test-helper';
-import global from '../global';
+import {
+	getContext,
+	testEnv,
+	testFs,
+	spyOnExec,
+	execCalledWith,
+} from '@technote-space/github-action-test-helper';
 import {
 	replaceDirectory,
 	cloneForBranch,
 	checkBranch,
 	prepareFiles,
 	createBuildInfoFile,
-	getCurrentBranchName,
 	copyFiles,
 	config,
-	commit,
 	push,
 } from '../../src/utils/command';
 
-let exists = false;
-beforeAll(() => {
-	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	const fs = require('fs');
-	jest.spyOn(fs, 'writeFileSync').mockImplementation(jest.fn());
-	jest.spyOn(fs, 'mkdirSync').mockImplementation(jest.fn());
-	jest.spyOn(fs, 'existsSync').mockImplementation(() => exists);
-});
-
-afterAll(() => {
-	jest.restoreAllMocks();
-});
+const setExists = testFs();
 
 describe('replaceDirectory', () => {
 	testEnv();
 
+	const workDir = path.resolve('test-dir/.work');
+	const buildDir = path.resolve('test-dir/.work/build');
+	const pushDir = path.resolve('test-dir/.work/push');
+
 	it('should replace build directory', () => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const buildDir = path.resolve('test-dir/.work/build');
 
 		expect(replaceDirectory(`git -C ${buildDir} fetch`)).toBe('git fetch');
 	});
 
 	it('should replace build directory', () => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const pushDir = path.resolve('test-dir/.work/push');
 
 		expect(replaceDirectory(`git -C ${pushDir} fetch`)).toBe('git fetch');
 	});
 
 	it('should replace working directory', () => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const workDir = path.resolve('test-dir/.work');
 
 		expect(replaceDirectory(`git -C ${workDir} fetch`)).toBe('git fetch');
 	});
 
 	it('should replace directories', () => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const workDir = path.resolve('test-dir/.work');
-		const buildDir = path.resolve('test-dir/.work/build');
-		const pushDir = path.resolve('test-dir/.work/push');
 
 		expect(replaceDirectory(`abc ${buildDir} && pqr ${workDir}/xyz ${pushDir}/123`)).toBe('abc <Build Directory> && pqr <Working Directory>/xyz <Push Directory>/123');
 	});
@@ -70,7 +59,7 @@ describe('cloneForBranch', () => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.INPUT_BRANCH_NAME = 'test-branch';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await cloneForBranch(getContext({
 			repo: {
@@ -80,8 +69,9 @@ describe('cloneForBranch', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(1);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} clone --branch=test-branch --depth=3 https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1 || :`);
+		execCalledWith(mockExec, [
+			`git -C ${dir} clone --branch=test-branch --depth=3 https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1 || :`,
+		]);
 	});
 });
 
@@ -92,57 +82,52 @@ describe('checkBranch', () => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.INPUT_BRANCH_NAME = 'test-branch';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await checkBranch('test-branch');
 
-		expect(execMock).not.toBeCalled();
+		expect(mockExec).not.toBeCalled();
 	});
 
 	it('should run git init command', async() => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.INPUT_BRANCH_NAME = 'test-branch';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await checkBranch('test-branch2');
 
 		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(3);
-		expect(execMock.mock.calls[0][0]).toBe(`rm -rdf ${dir}`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} init .`);
-		expect(execMock.mock.calls[2][0]).toBe(`git -C ${dir} checkout --orphan "test-branch"`);
+		execCalledWith(mockExec, [
+			`rm -rdf ${dir}`,
+			`git -C ${dir} init .`,
+			`git -C ${dir} checkout --orphan "test-branch"`,
+		]);
 	});
 });
 
 describe('prepareFiles', () => {
 	testEnv();
 
-	const commonCheck = (start: number, dir: string, execMock): void => {
-		expect(execMock.mock.calls[start][0]).toBe('yarn install --production');
-		expect(execMock.mock.calls[start][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 1][0]).toBe('rm -rdf .[!.]*');
-		expect(execMock.mock.calls[start + 1][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 2][0]).toBe('rm -rdf __tests__');
-		expect(execMock.mock.calls[start + 2][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 3][0]).toBe('rm -rdf src');
-		expect(execMock.mock.calls[start + 3][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 4][0]).toBe('rm -rdf *.js');
-		expect(execMock.mock.calls[start + 4][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 5][0]).toBe('rm -rdf *.ts');
-		expect(execMock.mock.calls[start + 5][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 6][0]).toBe('rm -rdf *.json');
-		expect(execMock.mock.calls[start + 6][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 7][0]).toBe('rm -rdf *.lock');
-		expect(execMock.mock.calls[start + 7][1]).toEqual({cwd: dir});
-		expect(execMock.mock.calls[start + 8][0]).toBe('rm -rdf _config.yml');
-		expect(execMock.mock.calls[start + 8][1]).toEqual({cwd: dir});
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const commonCheck = (dir: string): (string | any[])[] => {
+		return [
+			['yarn install --production', {cwd: dir}],
+			['rm -rdf .[!.]*', {cwd: dir}],
+			['rm -rdf __tests__', {cwd: dir}],
+			['rm -rdf src', {cwd: dir}],
+			['rm -rdf *.js', {cwd: dir}],
+			['rm -rdf *.ts', {cwd: dir}],
+			['rm -rdf *.json', {cwd: dir}],
+			['rm -rdf *.lock', {cwd: dir}],
+			['rm -rdf _config.yml', {cwd: dir}],
+		];
 	};
 
 	it('should run commands', async() => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await prepareFiles(getContext({
 			repo: {
@@ -154,18 +139,17 @@ describe('prepareFiles', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/build');
-		expect(execMock).toBeCalledTimes(12);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} clone --depth=3 https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} fetch https://octocat:test-token@github.com/Hello/World.git refs/heads/test > /dev/null 2>&1`);
-		expect(execMock.mock.calls[2][0]).toBe(`git -C ${dir} checkout -qf test-sha`);
-
-		commonCheck(3, dir, execMock);
+		execCalledWith(mockExec, ([
+			`git -C ${dir} clone --depth=3 https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`,
+			`git -C ${dir} fetch https://octocat:test-token@github.com/Hello/World.git refs/heads/test > /dev/null 2>&1`,
+			`git -C ${dir} checkout -qf test-sha`,
+		] as any[]).concat(commonCheck(dir))); // eslint-disable-line @typescript-eslint/no-explicit-any
 	});
 
 	it('should checkout branch', async() => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await prepareFiles(getContext({
 			repo: {
@@ -176,17 +160,16 @@ describe('prepareFiles', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/build');
-		expect(execMock).toBeCalledTimes(11);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} clone https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} checkout -qf test`);
-
-		commonCheck(2, dir, execMock);
+		execCalledWith(mockExec, ([
+			`git -C ${dir} clone https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`,
+			`git -C ${dir} checkout -qf test`,
+		] as any[]).concat(commonCheck(dir))); // eslint-disable-line @typescript-eslint/no-explicit-any
 	});
 
 	it('should checkout tag', async() => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await prepareFiles(getContext({
 			repo: {
@@ -197,11 +180,10 @@ describe('prepareFiles', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/build');
-		expect(execMock).toBeCalledTimes(11);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} clone https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} checkout -qf refs/tags/test`);
-
-		commonCheck(2, dir, execMock);
+		execCalledWith(mockExec, ([
+			`git -C ${dir} clone https://octocat:test-token@github.com/Hello/World.git . > /dev/null 2>&1`,
+			`git -C ${dir} checkout -qf refs/tags/test`,
+		] as any[]).concat(commonCheck(dir))); // eslint-disable-line @typescript-eslint/no-explicit-any
 	});
 });
 
@@ -249,7 +231,7 @@ describe('createBuildInfoFile', () => {
 		const mkdirMock = jest.spyOn(require('fs'), 'mkdirSync');
 		// eslint-disable-next-line @typescript-eslint/no-var-requires
 		const writeMock = jest.spyOn(require('fs'), 'writeFileSync');
-		exists = true;
+		setExists(true);
 
 		await createBuildInfoFile(getContext({
 			eventName: 'push',
@@ -258,103 +240,36 @@ describe('createBuildInfoFile', () => {
 
 		expect(mkdirMock).toBeCalledTimes(0);
 		expect(writeMock).toBeCalledTimes(1);
-
-		exists = false;
-	});
-});
-
-describe('getCurrentBranchName', () => {
-	afterEach(() => {
-		global.mockChildProcess.stdout = 'stdout';
-	});
-
-	it('should return empty', async() => {
-		exists = false;
-
-		expect(await getCurrentBranchName()).toBe('');
-	});
-
-	it('should return branch name', async() => {
-		global.mockChildProcess.stdout = 'test-branch';
-		exists = true;
-
-		expect(await getCurrentBranchName()).toBe('test-branch');
 	});
 });
 
 describe('copyFiles', () => {
 	it('should run rsync command', async() => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await copyFiles();
 
 		const buildDir = path.resolve('test-dir/.work/build');
 		const pushDir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(1);
-		expect(execMock.mock.calls[0][0]).toBe(`rsync -rl --exclude .git --delete "${buildDir}/" ${pushDir}`);
+		execCalledWith(mockExec, [
+			`rsync -rl --exclude .git --delete "${buildDir}/" ${pushDir}`,
+		]);
 	});
 });
 
 describe('config', () => {
 	it('should run git config command', async() => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await config();
 
 		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(2);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} config user.name "GitHub Actions"`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} config user.email "example@example.com"`);
-	});
-});
-
-describe('commit', () => {
-	afterEach(() => {
-		global.mockChildProcess.stdout = 'stdout';
-	});
-
-	it('should return false if there is no diff', async() => {
-		process.env.GITHUB_WORKSPACE = 'test-dir';
-		global.mockChildProcess.stdout = '';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
-		const stdoutMock = jest.spyOn(global.mockStdout, 'write');
-
-		expect(await commit()).toBeFalsy();
-
-		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(2);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} add --all --force`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} status --short -uno`);
-		expect(stdoutMock).toBeCalledTimes(3);
-		expect(stdoutMock.mock.calls[0][0]).toBe('[command]git add --all --force' + EOL);
-		expect(stdoutMock.mock.calls[1][0]).toBe('[command]git status --short -uno' + EOL);
-		expect(stdoutMock.mock.calls[2][0]).toBe('> There is no diff.' + EOL);
-	});
-
-	it('should return true', async() => {
-		process.env.GITHUB_WORKSPACE = 'test-dir';
-		global.mockChildProcess.stdout = 'A test.txt';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
-		const stdoutMock = jest.spyOn(global.mockStdout, 'write');
-
-		expect(await commit()).toBeTruthy();
-
-		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(4);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} add --all --force`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} status --short -uno`);
-		expect(execMock.mock.calls[2][0]).toBe(`git -C ${dir} commit -qm "feat: Build for release"`);
-		expect(execMock.mock.calls[3][0]).toBe(`git -C ${dir} show --stat-count=10 HEAD`);
-		expect(stdoutMock).toBeCalledTimes(7);
-		expect(stdoutMock.mock.calls[0][0]).toBe('[command]git add --all --force' + EOL);
-		expect(stdoutMock.mock.calls[1][0]).toBe('  >> A test.txt' + EOL);
-		expect(stdoutMock.mock.calls[2][0]).toBe('[command]git status --short -uno' + EOL);
-		expect(stdoutMock.mock.calls[3][0]).toBe('[command]git commit -qm "feat: Build for release"' + EOL);
-		expect(stdoutMock.mock.calls[4][0]).toBe('  >> A test.txt' + EOL);
-		expect(stdoutMock.mock.calls[5][0]).toBe('[command]git show --stat-count=10 HEAD' + EOL);
-		expect(stdoutMock.mock.calls[6][0]).toBe('  >> A test.txt' + EOL);
+		execCalledWith(mockExec, [
+			`git -C ${dir} config user.name "GitHub Actions"`,
+			`git -C ${dir} config user.email "example@example.com"`,
+		]);
 	});
 });
 
@@ -365,7 +280,7 @@ describe('push', () => {
 		process.env.INPUT_GITHUB_TOKEN = 'test-token';
 		process.env.GITHUB_WORKSPACE = 'test-dir';
 		process.env.INPUT_BRANCH_NAME = 'test-branch';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await push(getContext({
 			eventName: 'push',
@@ -377,16 +292,17 @@ describe('push', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(9);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2.3 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[2][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[3][0]).toBe(`git -C ${dir} tag -l | xargs git -C ${dir} tag -d`);
-		expect(execMock.mock.calls[4][0]).toBe(`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`);
-		expect(execMock.mock.calls[5][0]).toBe(`git -C ${dir} tag v1.2.3`);
-		expect(execMock.mock.calls[6][0]).toBe(`git -C ${dir} tag v1`);
-		expect(execMock.mock.calls[7][0]).toBe(`git -C ${dir} tag v1.2`);
-		expect(execMock.mock.calls[8][0]).toBe(`git -C ${dir} push --tags "https://octocat:test-token@github.com/Hello/World.git" "test-branch":"refs/heads/test-branch" > /dev/null 2>&1`);
+		execCalledWith(mockExec, [
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2.3 > /dev/null 2>&1 || :`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1 > /dev/null 2>&1 || :`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2 > /dev/null 2>&1 || :`,
+			`git -C ${dir} tag -l | xargs git -C ${dir} tag -d`,
+			`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`,
+			`git -C ${dir} tag v1.2.3`,
+			`git -C ${dir} tag v1`,
+			`git -C ${dir} tag v1.2`,
+			`git -C ${dir} push --tags "https://octocat:test-token@github.com/Hello/World.git" "test-branch":"refs/heads/test-branch" > /dev/null 2>&1`,
+		]);
 	});
 
 	it('should run git push command with pushing original tag', async() => {
@@ -394,7 +310,7 @@ describe('push', () => {
 		process.env.GITHUB_WORKSPACE = 'test-dir';
 		process.env.INPUT_BRANCH_NAME = 'test-branch';
 		process.env.INPUT_ORIGINAL_TAG_PREFIX = 'original/';
-		const execMock = jest.spyOn(global.mockChildProcess, 'exec');
+		const mockExec = spyOnExec();
 
 		await push(getContext({
 			eventName: 'push',
@@ -406,18 +322,21 @@ describe('push', () => {
 		}));
 
 		const dir = path.resolve('test-dir/.work/push');
-		expect(execMock).toBeCalledTimes(12);
-		expect(execMock.mock.calls[0][0]).toBe(`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`);
-		expect(execMock.mock.calls[1][0]).toBe(`git -C ${dir} tag original/v1.2.3 v1.2.3`);
-		expect(execMock.mock.calls[2][0]).toBe(`git -C ${dir} push "https://octocat:test-token@github.com/Hello/World.git" "refs/tags/original/v1.2.3" > /dev/null 2>&1`);
-		expect(execMock.mock.calls[3][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2.3 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[4][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[5][0]).toBe(`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2 > /dev/null 2>&1 || :`);
-		expect(execMock.mock.calls[6][0]).toBe(`git -C ${dir} tag -l | xargs git -C ${dir} tag -d`);
-		expect(execMock.mock.calls[7][0]).toBe(`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`);
-		expect(execMock.mock.calls[8][0]).toBe(`git -C ${dir} tag v1.2.3`);
-		expect(execMock.mock.calls[9][0]).toBe(`git -C ${dir} tag v1`);
-		expect(execMock.mock.calls[10][0]).toBe(`git -C ${dir} tag v1.2`);
-		expect(execMock.mock.calls[11][0]).toBe(`git -C ${dir} push --tags "https://octocat:test-token@github.com/Hello/World.git" "test-branch":"refs/heads/test-branch" > /dev/null 2>&1`);
+		execCalledWith(mockExec, [
+			`git -C ${dir} tag -l | xargs git -C ${dir} tag -d`,
+			`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag original/v1.2.3 > /dev/null 2>&1 || :`,
+			`git -C ${dir} tag original/v1.2.3 v1.2.3`,
+			`git -C ${dir} push "https://octocat:test-token@github.com/Hello/World.git" "refs/tags/original/v1.2.3" > /dev/null 2>&1`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2.3 > /dev/null 2>&1 || :`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1 > /dev/null 2>&1 || :`,
+			`git -C ${dir} push --delete "https://octocat:test-token@github.com/Hello/World.git" tag v1.2 > /dev/null 2>&1 || :`,
+			`git -C ${dir} tag -l | xargs git -C ${dir} tag -d`,
+			`git -C ${dir} fetch "https://octocat:test-token@github.com/Hello/World.git" --tags > /dev/null 2>&1`,
+			`git -C ${dir} tag v1.2.3`,
+			`git -C ${dir} tag v1`,
+			`git -C ${dir} tag v1.2`,
+			`git -C ${dir} push --tags "https://octocat:test-token@github.com/Hello/World.git" "test-branch":"refs/heads/test-branch" > /dev/null 2>&1`,
+		]);
 	});
 });
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,25 +1,3 @@
-import global from './__tests__/global';
+import { setupGlobal } from '@technote-space/github-action-test-helper';
 
-global.mockStdout = {
-	write: jest.fn(),
-};
-process.stdout.write = global.mockStdout.write;
-
-global.mockChildProcess = {
-	stdout: 'stdout',
-	stderr: '',
-	exec: jest.fn((...args: any[]) => {
-		const callback = args.length === 2 ? args[1] : args[2];
-		callback(null, global.mockChildProcess.stdout, global.mockChildProcess.stderr);
-	}),
-};
-jest.mock('child_process', () => ({
-	...jest.requireActual('child_process'),
-	exec: global.mockChildProcess.exec,
-}));
-
-global.console.log = jest.fn();
-global.console.warn = jest.fn();
-global.console.error = jest.fn();
-
-process.env.GITHUB_ACTOR = 'octocat';
+setupGlobal();
